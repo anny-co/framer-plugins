035e98828de86ca77024d3b7f1339e4bb1ddef56
lucianholt97 <lucian@holtwiesche.de>
2025-08-25 09:58:45 +0200

    feat: add google sheets import plugin

---
+++ plugins/google-sheets-import/src/main.tsx
@@ -0,0 +1,27 @@
+import "framer-plugin/framer.css"
+
+import { framer } from "framer-plugin"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App.tsx"
+
+const root = document.getElementById("root")
+if (!root) throw new Error("Root element not found")
+
+const collection = await framer.getActiveCollection()
+
+if (!collection) {
+    await framer.closePlugin("Please select a Collection to import into")
+    throw new Error("Unreachable")
+}
+
+if (collection.readonly) {
+    await framer.closePlugin("Google Sheets Import can only be used on writable Collections")
+    throw new Error("Unreachable")
+}
+
+ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+        <App collection={collection} />
+    </React.StrictMode>
+)
